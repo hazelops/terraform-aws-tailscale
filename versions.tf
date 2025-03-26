fcc4097bd51e7b83c3487151f9531016c0e996eb
Dmitry Borisov <dmitry.borisov@hazelops.com>
2025-03-27 02:10:52 +0300

    Providers local and template removed/Readme updated/data replaced with templatefile

--- versions.tf
+++ versions.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=4.30.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">=2.2"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 1.2"
-    }
     tailscale = {
       source  = "tailscale/tailscale"
       version = "0.13.13"
